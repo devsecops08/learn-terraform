c3ec08a6798273baf7620e436f13e1383d4852d2
Your Name <you@example.com>
2024-11-25 12:00:17 +0530

    add

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,5 +1,10 @@
 resource "aws_instance""frontend" {
           ami ="ami-09c813fb71547fc4f"
           instance_type = "t3.small"
+          vpc_security_group_ids=["sg-049f3b7cfdfab115b"]
+          tags={
+            name="frontend"
+
+          }
   
 }
