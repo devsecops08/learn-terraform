3a5521dc929856a7ef39847ee34637c061f40a1b
Your Name <you@example.com>
2024-11-28 15:33:51 +0530

    add

--- 02-variables/main.tf
+++ 02-variables/main.tf
@@ -24,3 +24,34 @@ variable "str1" {
   default = "Hello"
 }
 # numbers,booleans & strings
+#variable types
+variable "str2" {
+  default = "WORLD"
+}
+variable "list1" {
+  default = [
+    10,
+    20,
+    xyz,
+    false
+  ]
+}
+# list can have a multiple data types also
+variable "map1" {
+  default = {
+    aws ={
+      trainer="john"
+      duration="30"
+    }
+    devops={
+        trainer="steve"
+        duration="30"
+    }
+  }
+}
+output "list1_1" {
+  value = var.list1[2]
+}
+output "map1_aws_trainer" {
+value =var.map1["aws"]["trainer"]
+}
