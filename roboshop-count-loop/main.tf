e691d529e31a465c0119cecf0c37f4d8f4001295
Your Name <you@example.com>
2024-11-29 12:48:11 +0530

    add

---
+++ roboshop-count-loop/main.tf
@@ -0,0 +1,33 @@
+variable "instances" {
+    default = [
+        "frontend",
+        "mongodb",
+        "catalogue",
+        "cart",
+        "user",
+        "redis",
+        "mysql",
+        "shipping",
+        "rabbitmq",
+        "payment"
+    ] 
+}
+resource "aws_instance""instance" {
+          count =length(var.instances)
+          ami ="ami-09c813fb71547fc4f"
+          instance_type = "t3.small"
+          vpc_security_group_ids=["sg-049f3b7cfdfab115b"]
+          tags= {
+            name=var.instances[count.index]
+
+          } 
+}
+
+resource "aws_route53_record" "frontend" {
+  count =length(var.instances)
+  zone_id = "Z04036722QT9R780VLSOQ"
+  name    = "frontend-dev.adevsecops08.online"
+  type    = "A"
+  ttl     = "30"
+  records = [aws_instance.instance[count.index].private_ip]
+}
