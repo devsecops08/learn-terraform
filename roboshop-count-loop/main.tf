cce80e7b56dc4ade88570fba5d96e8dd371f08ef
Your Name <you@example.com>
2024-11-29 13:50:38 +0530

    add

--- roboshop-count-loop/main.tf
+++ roboshop-count-loop/main.tf
@@ -12,13 +12,13 @@ variable "instances" {
         "payment"
     ] 
 }
-resource "aws_instance""instance" {
+resource "aws_instance" "instance" {
           count =length(var.instances)
           ami ="ami-09c813fb71547fc4f"
           instance_type = "t3.small"
           vpc_security_group_ids=["sg-049f3b7cfdfab115b"]
           tags= {
-            name=var.instances[count.index]
+            Name=var.instances[count.index]
      
           } 
 }
