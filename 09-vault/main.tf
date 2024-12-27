c1fadb493e3a66a4c49528c50c35ee0d19cc44f2
Your Name <you@example.com>
2024-12-27 18:08:15 +0530

    add

--- 09-vault/main.tf
+++ 09-vault/main.tf
@@ -11,19 +11,16 @@ data "vault_generic_secret" "example" {
   path = "test/my_credentials"
 }
 
- #data "vault_kv_secret_v2" "example" {
-  #name = "my_credentials"
- #mount = "test"
-#
+# data "vault_kv_secret_v2" "example" {
+#   name  = "my_credentials"
+#   mount = "test"
+# }
 
-#resource "local_file" "foo" {
- # content  = data.vault_generic_secret.example.data["password"]
-  #filename = "/tmp/secret"
-#}
 resource "local_file" "foo" {
- content  = data.vault_kv_secret_v2.example.data_json["password"]
+  content  = data.vault_generic_secret.example.data["password"]
   filename = "/tmp/secret"
 }
-#output "json" {["password"]
- # value = data.vault_kv_secret_v2.example.data_json
-#
+
+# output "json" {
+#   value = data.vault_kv_secret_v2.example.data_json
+# }
